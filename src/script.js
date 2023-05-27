49378e309c0e504dc93d69c7968cf37e00121d14
daryavolnaya <129992395+daryavolnaya@users.noreply.github.com>
2023-05-27 15:38:33 +0200

    Filled about and project page

--- src/script.js
+++
@@ -1,8 +0,0 @@
-function open_photo(photo) {
-  document.getElementById("big-photo").innerHTML =
-    ("<img onclick='close_photo()' style='position: absolute;' src='" + photo + "'class='max-photo'>")
-}
-
-function close_photo() {
-  document.getElementById("big-photo").innerHTML = ""
-}
